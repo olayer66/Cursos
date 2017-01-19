692e79fde76b706516a65c659b881a9cd93a4d97
usuario_local <usuario_local@PTO0219>
2017-01-19 18:16:59 +0100

    que cuki

--- Cursos/static/js/gestionCursos.js
+++ Cursos/static/js/gestionCursos.js
@@ -6,6 +6,7 @@
 //Carga en pantalla los cursos extraidos
 function mostrarCursos(cursos)
 {
+    //INFORMACION: poner que dependiendo de la fecha de final se ponga de un color u otro
     borrarTablaCursos();
     cursos.forEach(function(curso)
     {
@@ -24,15 +25,25 @@ function mostrarCursos(cursos)
 //inserta tantos botones de paginas como sea necesario
 function insertarPaginacion(total)
 {
-    alert("tenemos paginacion");
+    var numPags= Math.ceil(total/5);
+    alert(total+"|||"+numPags);
+    $("#menuPaginacion").append("<li class='botonMini' id='unoAtras'> << </li>");
+    for(var i=1;i<=numPags;i++)
+    {
+        var posInicio=(i-1)*5;
+        $("#menuPaginacion").append("<li class='botonMini' id='botonPagina' data-posInicio='"+posInicio+"'>"+i+"</li>");
+    }
+    $("#menuPaginacion").append("<li class='botonMini' id='unoAlante'> >> </li>");
 }
 /*==========================FUNCIONES AUXILIARES===========================*/
+//Extrae la fechea de una variable
 function extraerFecha(fecha)
 {
     var date= new Date(fecha);
     var mes=date.getMonth()+1;
     return date.getDate()+"/"+mes+"/"+date.getFullYear();
 }
+//borra los filas de la tabla de busyqeda de cursos
 function borrarTablaCursos()
 {
     $(".filaCurso").remove();
